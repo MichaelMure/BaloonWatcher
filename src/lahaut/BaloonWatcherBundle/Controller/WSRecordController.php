ec520adb3c719392a11720070b2a611c4c017501
abour <ayorosmage@gmail.com>
2013-05-25 17:17:02 +0200

    Web service implementation relative to : GPSRecord creation, Scenario creation and gpsPointList retrival according last scenario

---
+++ src/lahaut/BaloonWatcherBundle/Controller/WSRecordController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: alexandrebour
+ * Date: 25/05/13
+ * Time: 14:02
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace lahaut\BaloonWatcherBundle\Controller;
+
+
+class WSRecordController {
+
+}
