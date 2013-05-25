254cf8a9a6e0e3185ae814e73aa7f34d7207316c
abour <ayorosmage@gmail.com>
2013-05-25 19:24:56 +0200

    MainPage + bootstrap integration

---
+++ src/lahaut/BaloonWatcherBundle/Controller/MonitoringController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace lahaut\BaloonWatcherBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class MonitoringController extends Controller{
+
+    /**
+     * @Route("/index")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction()
+    {
+        return $this->render('BaloonWatcherBundle:Default:monitoring.html.twig');
+    }
+
+}
