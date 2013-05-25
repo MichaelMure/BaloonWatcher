f0824941fa98694b23f99875faa06bde5f23e386
abour <ayorosmage@gmail.com>
2013-05-26 00:06:34 +0200

    javascript implementation for drawing placemark

--- src/lahaut/BaloonWatcherBundle/Resources/public/js/MonitoringPageController.js
+++ src/lahaut/BaloonWatcherBundle/Resources/public/js/MonitoringPageController.js
@@ -1,16 +1,30 @@
-$(document).ready(function() {
-    var loadGpsPointList = function () {
-        $.get('ajax/test.html', function(data) {
-            $('.result').html(data);
-            alert('Load was performed.');
-        });
-    }
+var $gpsPointList = null;
+var ge = null;
 
-    console.log(Routing.generate("lahaut_baloonwatcher_wsrecord_gpspointlistforlastscenario"));
+$(document).ready(function() {
+    loadGpsPointList();
 });
 
 google.load("earth", "1");
 google.setOnLoadCallback(init);
+
+var loadGpsPointList = function () {
+    var gpsPointListUrl = Routing.generate("lahaut_baloonwatcher_wsrecord_gpspointlistforlastscenario");
+
+    $.get(gpsPointListUrl, function(data) {
+        $gpsPointList = data;
+    });
+}
+
+var updateGpsPointList = function () {
+    var gpsPointListUrl = Routing.generate("lahaut_baloonwatcher_wsrecord_gpspointlistforlastscenario");
+
+    $.get(gpsPointListUrl, function(data) {
+        $gpsPointList = data;
+
+        displayGpsPointList();
+    });
+}
 
 function init() {
     google.earth.createInstance('map3d', initCB, failureCB);
@@ -20,26 +34,37 @@ function initCB(instance) {
     ge = instance;
     ge.getWindow().setVisibility(true);
 
-    var i;
+    displayGpsPointList();
+    setInterval(updateGpsPointList, 2000);
+}
 
-    for( i=0 ; i<10 ; i++) {
+function displayGpsPointList() {
+    removeAllPlacemark();
+    for (var key in $gpsPointList) {
         // Create the placemark.
         var placemark = ge.createPlacemark('');
 
         // Set the placemark's location.
         var point = ge.createPoint('');
-        point.setLatitude(48.036331);
-        point.setLongitude(7.122809);
+        point.setLatitude($gpsPointList[key].latitude);
+        point.setLongitude($gpsPointList[key].longitude);
         point.setAltitudeMode(ge.ALTITUDE_ABSOLUTE);
-        point.setAltitude(395 + 30 * i);
+        point.setAltitude(395);
         placemark.setGeometry(point);
 
         // Add the placemark to Earth.
         ge.getFeatures().appendChild(placemark);
     }
-
 }
 
 function failureCB(errorCode) {
     console.log("failure");
 }
+
+function removeAllPlacemark() {
+    var features = ge.getFeatures();
+    while (features.getFirstChild()) {
+        features.removeChild(features.getFirstChild());
+    }
+}
+
